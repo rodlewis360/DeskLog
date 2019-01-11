a9f6b2bbdcb523dfb7cc69d2a84e720260517d97
rodlewis360 <45081643+rodlewis360@users.noreply.github.com>
2019-01-11 12:13:19 -0800

    updates

--- desklog1.py
+++ desklog1.py
@@ -15,67 +15,67 @@ def logprocess(log):
     print("What would you like to put in your log?")
     log += input("")
     return log
-    
-    
-while True:
-    print("What would you like to do?")
-    whattodo = input("")
-    if whattodo == "new":
-        Log = Logprocessstart()
-        logsfilled += 1
-        LogList.append(Log)
-        Log.append(logsfilled)
-    if whattodo == "open":
-        print("What log would you like to open?")
+
+def DeskLog(logsfilled):
+    while True:
+        print("What would you like to do?")
         whattodo = input("")
-        x = 0
-        thislog = ""
-        for a in LogList:
-            if len(a) != 0:
-                if a[0] == whattodo:
-                    thislog = a[1]
-                    logprocess(thislog)
-                    break
-            x += 1
-        if thislog == "":
-            print("Error: Log with name:", whattodo, "not found")
-    if whattodo == "merge":
-        print("What files?(One at a time)")
-        fileone = input("")
-        filetwo = input("")
-        thislog = ""
-        x = 0
-        for a in LogList:
-            if len(a) != 0:
-                if a[0] == whattodo:
-                    thislog = a[1]
-            x += 1
-        if thislog == "":
-            print("Error: Log with name:",fileone,"not found")
-        thislogone = ""
-        for a in LogList:
-            if len(a) != 0:
-                if a[0] == whattodo:
-                    thislogone = a[1]
-        if thislogone == "":
-            print("Error: Log with name:", filetwo, "not found")
-        thislog += thislogone
-        LogList[x] = thislog
-    if whattodo == "delete":
-        print("What log would you like to delete?")
-        fileone = input("")
-        x = 0
-        for a in LogList:
-            if len(a) != 0:
-                if a[0] == fileone:
-                    if len(LogList) > 1:
-                        LogList[x] = LogList[x + 1]
-                    else:
+        if whattodo == "new":
+            Log = Logprocessstart()
+            logsfilled += 1
+            LogList.append(Log)
+            Log.append(logsfilled)
+        if whattodo == "open":
+            print("What log would you like to open?")
+            whattodo = input("")
+            x = 0
+            thislog = ""
+            for a in LogList:
+                if len(a) != 0:
+                    if a[0] == whattodo:
+                        thislog = a[1]
+                        thislog = logprocess(thislog)
+                        LogList[x] = [a[0], thislog, a[2]]
+                        break
+                x += 1
+            if thislog == "":
+                print("Error: Log with name:", whattodo, "not found")
+        if whattodo == "merge":
+            print("What files?(One at a time)")
+            fileone = input("")
+            filetwo = input("")
+            thislog = ""
+            x = 0
+            for a in LogList:
+                if len(a) != 0:
+                    if a[0] == whattodo:
+                        thislog = a[1]
+                        break
+                x += 1
+            if thislog == "":
+                print("Error: log with name", fileone, "not found.")
+            thislogone = ""
+            for a in LogList:
+                if len(a) != 0:
+                    if a[0] == whattodo:
+                        thislogone = a[1]
+                        thislog += thislogone
+                        if thislog != "":
+                            LogList[x] = [a[0], thislog, a[2]]
+                        break
+            if thislogone == "":
+                print("Error: Log with name",filetwo,"not found.")
+        if whattodo == "delete":
+            print("What log would you like to delete?")
+            fileone = input("")
+            x = 0
+            for a in LogList:
+                if len(a) != 0:
+                    if a[0] == fileone:
                         LogList[x] = ""
-            x += 1
-    if whattodo == "show":
-        print("Name                                        ID")
-        for a in LogList:
-            if len(a) > 2:
-                print(a[0],"                                   ",a[2])
-        
+                x += 1
+        if whattodo == "show":
+            print("Name                                        ID")
+            for a in LogList:
+                if len(a) > 2:
+                    print(a[0],"                                   ",a[2])
